6cc1f438370e01206add0142bd948a6e4952c25a
cebed <36077080+cebed@users.noreply.github.com>
2019-03-26 21:20:39 +0100

    frontend

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {ButtonToolbar, Button} from 'react-bootstrap';
+import Bootstap from 'bootstrap/dist/css/bootstrap.css';
+
+
+class App extends Component {
+    render() {
+        return (
+
+            <div className="App">
+                <header className="App-header">
+                    <div className="Btn1">
+                        <ButtonToolbar>
+                            <Button variant="primary">VI</Button>
+                            <Button variant="secondary">SKAAA</Button>
+                            <Button variant="success">GREJAA</Button>
+                            <Button variant="danger">EXJOBB</Button>
+                        </ButtonToolbar>
+                    </div>
+
+
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+
+
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
