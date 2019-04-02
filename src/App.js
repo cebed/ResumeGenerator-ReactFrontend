02083554a8c02249aa22fa1539e170251b2597ca
cebed <36077080+cebed@users.noreply.github.com>
2019-04-02 15:58:51 +0200

    Revert "imported dependencies, added component"
    This reverts commit 7898bbee3fb2e6ab2eff35fc5590742c6c10ce81.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import "bootstrap/dist/css/bootstrap.min.css"
 import {Form, Button} from 'react-bootstrap';
 import Header from "./components/header";
 import Footer from "./components/footer";
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {Button, Form} from "react-bootstrap";
 import Nav from "./nav";
-import Loginform from "./loginform"
 
 
 class Header extends Component {
@@ -18,7 +17,23 @@ class Header extends Component {
             <div className="parag">
               <p>Do you want to generate your resume? Or do you want to view your CV and maybe download it? <br/>Let's go!</p>
 
-               <Loginform/>
+                <Form className="logier">
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+
+                    <Button className="login" type="submit">
+                        Login
+                    </Button>
+
+                </Form>
             </div>
         </div>
        
