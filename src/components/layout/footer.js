e955d58ca46d62bd9a7fe9fd6c5235633b3f20fe
cebed <36077080+cebed@users.noreply.github.com>
2019-04-08 20:55:21 +0200

    unused components

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,75 +1,13 @@
 import React, { Component } from 'react';
-import {Button, Form} from "react-bootstrap";
-import axios from 'axios';
+
 
 class Footer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmittRegister= this.handleSubmittRegister.bind(this);
-
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value, Person: []},
-        );
-    }
-
-    handleSubmittRegister(event) {
-
-        event.preventDefault();
-        const {value} = this.state
-        const config = {
-            headers: {
-                'content-type': 'multi-part/form-data'
-            }
-        };
-
-
-        axios.post(`http://ec2-3-9-17-175.eu-west-2.compute.amazonaws.com:5004/login`,  value , config )
-            .then((response) =>{
-                if(response.status === 200){
-                    alert('Logged in as : ' + response.data );
-                }
-
-            });
-    }
-
-    componentDidMount() {
-        axios.get(`http://ec2-3-9-17-175.eu-west-2.compute.amazonaws.com:5004/all` )
-            .then(res=> {
-                console.log(res)
-                if(res.status === 200){
-                    /* alert('A name was submitted: failed' );*/
-                }
-                this.setState({Person: res.data})
-            })
-
-
-    }
   render() {
     return (
      
       <footer>
-          <Form className="logier" onSubmit={this.handleSubmittRegister}>
-              <Form.Group controlId="formBasicEmail">
-                  <Form.Label>RLogin only by email </Form.Label>
-                  <Form.Control  type="text" value={this.state.value} onChange={this.handleChange} />
-              </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-
-
-              <Button className="login" type="submit">
-                  Login
-              </Button>
-
-          </Form>
           <ul>
               <li className="logo">By<span>Chalmers</span>Students</li>
           </ul>
