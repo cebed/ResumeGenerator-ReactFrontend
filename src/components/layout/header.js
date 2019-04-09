fca7d950dc1872a0dc420cf11ee82943b93c5ae2
cebed <36077080+cebed@users.noreply.github.com>
2019-04-09 23:40:59 +0200

    works fine now

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -2,16 +2,75 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import {Dropdown} from "react-bootstrap";
 import Bootstap from 'bootstrap/dist/css/bootstrap.min.css';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logout } from "../../actions/securityActions";
 
 
 class Header extends Component {
+    logout() {
+        this.props.logout();
+        window.location.href = "/";
+    }
     render() {
+        const { validToken, user } = this.props.security;
+
+        const userIsAuthenticated = (
+            <div className="collapse navbar-collapse" id="mobile-nav">
+
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">
+                            <i className="fas fa-user-circle mr-1" />
+                            {user.fullName}
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            to="/logout"
+                            onClick={this.logout.bind(this)}
+                        >
+                            Logout
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        );
+
+
+        const userIsNotAuthenticated = (
+            <div className="collapse navbar-collapse" id="mobile-nav">
+                <ul className="navbar-nav ml-auto">
+
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/Register">
+                            Sign Up
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/Login">
+                            Login
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        );
+
+        let headerLinks;
+
+        if (validToken && user) {
+            headerLinks = userIsAuthenticated;
+        } else {
+            headerLinks = userIsNotAuthenticated;
+        }
+
         return (
             <nav className="navbar navbar-expand-xl navbar-dark bg-dark mb-4">
                 <div className="container">
                     <ul>
                         <li className="logo">Resume<span>Generator</span></li>
-                        <Link to="/"> <h2 className="centeritem"> Welcome </h2> </Link>
+                        <Link className="navbar-brand" to="/"><h2 className="navbarwelcome">Welcome</h2> </Link>
                     </ul>
 
 
@@ -19,6 +78,8 @@ class Header extends Component {
 
 
                         <ul className="navbar-nav ml-auto">
+                            {headerLinks}
+
 
                             <li className="nav-item">
                                 <Dropdown>
@@ -34,6 +95,7 @@ class Header extends Component {
                                 </Dropdown>
                             </li>
                         </ul>
+
                     </div>
                 </div>
             </nav>
@@ -41,4 +103,17 @@ class Header extends Component {
     }
 }
 
-export default Header;
+Header.propTypes = {
+    logout: PropTypes.func.isRequired,
+    security: PropTypes.object.isRequired
+};
+
+
+const mapStateToProps = state => ({
+    security: state.security
+});
+
+export default connect(
+    mapStateToProps,
+    { logout }
+)(Header)
