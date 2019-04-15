24e6f737744e4de578cd12ded1f68bc697b9b4ea
nurhusein <nurabd@student.chalmers.se>
2019-04-15 09:58:25 +0200

    added an action class for workEx

--- src/actions/WorkExActions.js
+++ src/actions/WorkExActions.js
@@ -1,17 +1,18 @@
 
 import axios from 'axios';
-
 export const FETCH_WorkEx = 'WorkEx';
-
-
-
-
-
 export const fetchWorkEx = () => async dispatch => {
-    const res = await axios.get(`http://localhost:5005/api/users/allWork`);
+    const res = await axios.get(`http://localhost:5005/api/work/all`);
     dispatch({
         type: FETCH_WorkEx,
         payload: res.data
     });
 };
+
+export const createNewEx = (newuser, history) => async dispatch => {
+        await axios.post("http://localhost:5005/api/work/register", newuser);
+        history.push("/ExperienceList");
+
+        };
+
 
