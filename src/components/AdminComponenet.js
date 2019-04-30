d0784b6c840456d4e6343026497a7a88e1a04ebb
cebed <36077080+cebed@users.noreply.github.com>
2019-04-30 18:20:59 +0200

    adminpage layout

--- src/components/AdminComponenet.js
+++ src/components/AdminComponenet.js
@@ -5,6 +5,11 @@ import {fechUsers} from "../actions/userActions";
 import { Link } from 'react-router-dom';
 import _ from "lodash";
 import CourseComponent from "./CourseComponent";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import CardDeck from "react-bootstrap/CardDeck";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
 
 class adminComponenet extends Component {
 
@@ -16,15 +21,47 @@ class adminComponenet extends Component {
     renderUsers(){
         return _.map(this.props.user.user, list => {
             return(
-                <li className="list-group-item" key={list.id}>
-                    <Link to={`/ResumeBoard/${list.id}`}>
-                       Name:  {list.fullName}   email : {list.username}
-                    </Link>
-                </li>
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="paddingCard">
+                          
 
-            )
+                <Card className="text-center" style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={list.image}  alt="Upload Image" />
+                    <Card.Body>
+                        <Card.Title>{list.fullName}</Card.Title>
+
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem>{list.username}</ListGroupItem>
+
+                    </ListGroup>
+                    <Card.Body>
+                        <Button href={`/ResumeBoard/${list.id}`}>
+
+                            View CV
+
+                        </Button>
+
+                    </Card.Body>
+                </Card>
+
+                    </div>
+                    </div></div>
+
+
+
+
+
+
+
+
+
+        )
         });
     }
+
+
 
 
 
