b259d3a34cb81a19141a914507733a05bf493d63
nurhusein <nurabd@student.chalmers.se>
2019-11-21 18:26:57 +0100

    chnged the addres again

--- src/Utils/environment.js
+++ src/Utils/environment.js
@@ -1,4 +1,4 @@
-let BASE_URL = "http://ec2-3-9-17-175.eu-west-2.compute.amazonaws.com:5005/api"
+let BASE_URL = "http://ec2-54-88-175-92.compute-1.amazonaws.com:5005/api";
 
 
 if (process.env.REACT_APP_ENV === "development") {
