c99abae946e71d7d25bdabc6efcc647507c4545c
nurhusein <nurabd@student.chalmers.se>
2019-11-24 14:58:09 +0100

    the header is been fixed

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,7 @@ import {Dropdown} from "react-bootstrap";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {logout} from "../actions/securityActions";
+import {Navbar,  Nav, NavItem, NavDropdown} from 'react-bootstrap'
 
 
 class Header extends Component {
@@ -12,35 +13,50 @@ class Header extends Component {
         window.location.href = "/";
     }
 
+
+
     render() {
         const {validToken, user} = this.props.security;
         console.log(user);
 
         const userIsAuthenticated = (
-            <div className="collapse navbar-collapse" id="mobile-nav">
+            <div>
+                {/** <div className="collapse navbar-collapse" id="mobile-nav">
 
-                <ul className="navbar-nav ml-auto">
+                 <ul className="navbar-nav ml-auto">
 
-                    <li className="nav-item">
-                        <Link to={`/ResumeBoard/${user.id}`}>
-                            <i className="fas fa-user-circle mr-1"/>
-                            <h6>{user.fullName}</h6>
-                        </Link>
-                    </li>
+                 <li className="nav-item">
+                 <Link to={`/ResumeBoard/${user.id}`}>
+                 <i className="fas fa-user-circle mr-1"/>
+                 <h6>{user.fullName}</h6>
+                 </Link>
+                 </li>
 
-                    <li className="nav-item">
-                        <Link
-                            className="nav-link"
-                            to="/logout"
-                            onClick={this.logout.bind(this)}
-                        >
-                            Logout
-                        </Link>
-                    </li>
-                </ul>
+                 <li className="nav-item">
+                 <Link
+                 className="nav-link"
+                 to="/logout"
+                 onClick={this.logout.bind(this)}
+                 >
+                 Logout
+                 </Link>
+                 </li>
+                 </ul>
 
-            </div>
+                 </div>**/}
+                <Nav>
+                    <Nav.Link href={`/ResumeBoard/${user.id}`}>
+                        <i className="fas fa-user-circle mr-1"> {user.fullName}</i>
 
+                    </Nav.Link>
+
+                    <Nav.Link  href="/"
+                               onClick={this.logout.bind(this)}>
+                        Logout
+                    </Nav.Link>
+                </Nav>
+
+    </div>
         );
 
 
@@ -51,19 +67,23 @@ class Header extends Component {
         const isadmin = (
 
             <div>
+                {/** <li className="nav-item">
+                 <Dropdown>
+                 <Dropdown.Toggle variant="success" id="dropdown-basic">
+                 Menu
+                 </Dropdown.Toggle>
 
-                <li className="nav-item">
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Menu
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="/AdminComponenet"> Search for employees</Dropdown.Item>
-                            <Dropdown.Item href="/EditFrontEdgeAddressPage">Edit company information</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </li>
+                 <Dropdown.Menu>
+                 <Dropdown.Item href="/AdminComponenet"> Search for employees</Dropdown.Item>
+                 <Dropdown.Item href="/EditFrontEdgeAddressPage">Edit company information</Dropdown.Item>
+                 </Dropdown.Menu>
+                 </Dropdown>
+                 </li>**/}
+                <NavDropdown title="Dropdown" id="collasible-nav-dropdown" variant="success">
+                    <NavDropdown.Item href="/AdminComponenet"> Search for employees</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="/EditFrontEdgeAddressPage">Edit company information</NavDropdown.Item>
+                </NavDropdown>
             </div>
 
         );
@@ -71,27 +91,38 @@ class Header extends Component {
 
         if (user.adminOrUser === true) {
             Admin = isadmin;
+
         } else {
             Admin = isNotadmin;
         }
 
 
         const userIsNotAuthenticated = (
-            <div className="collapse navbar-collapse" id="mobile-nav">
-                <ul className="navbar-nav ml-auto">
+            <div>
+            {/** <div className="collapse navbar-collapse" id="mobile-nav">
+                 <ul className="navbar-nav ml-auto">
 
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/Register">
-                            Sign Up
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">
-                            Login
-                        </Link>
-                    </li>
-                </ul>
-            </div>
+                 <li className="nav-item">
+                 <Link className="nav-link" to="/Register">
+                 Sign Up
+                 </Link>
+                 </li>
+                 <li className="nav-item">
+                 <Link className="nav-link" to="/">
+                 Login
+                 </Link>
+                 </li>
+                 </ul>
+                 </div>**/}
+                <Nav>
+                <Nav.Link href="/">login</Nav.Link>
+
+                <Nav.Link  href="/Register">
+                    Register
+                </Nav.Link>
+                </Nav>
+              </div>
+
         );
 
         let headerLinks;
@@ -104,26 +135,43 @@ class Header extends Component {
 
 
         return (
-            <nav className="navbar navbar-expand-xl navbar-dark bg-dark mb-4">
-                <div className="container">
-                    <ul>
-                        <li className="logo">Resume<span>Generator</span></li>
-                        <li className="navbarwelcome"><h2>FrontEdge<span>IT</span></h2></li>
-                    </ul>
+            <div>
+                {/**
+                 <nav className="navbar navbar-expand-xl navbar-dark bg-dark mb-4">
+                 <div className="container">
+                 <ul>
+                 <li className="logo">Resume<span>Generator</span></li>
+                 <li className="navbarwelcome"><h2>FrontEdge<span>IT</span></h2></li>
+                 </ul>
 
 
-                    <div className="collapse navbar-collapse" id="mobile-nav">
+                 <div className="collapse navbar-collapse" id="mobile-nav">
 
 
-                        <ul className="navbar-nav ml-auto">
-                            {headerLinks}
+                 <ul className="navbar-nav ml-auto">
+
+                 {Admin}
+
+                 </ul>
+
+                 </div>
+                 </div>
+                 </nav>**/}
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Navbar.Brand href="#home">Resume generator</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
                             {Admin}
 
-                        </ul>
+                        </Nav>
 
-                    </div>
-                </div>
-            </nav>
+                            {headerLinks}
+
+                    </Navbar.Collapse>
+                </Navbar>
+
+            </div>
         );
     }
 }
